4508b07d0c6a905532ea9d30903892ec86cda74f
slemchik03 <danylo@talio.ai>
2026-05-10 13:15:44 +0300

    feat: fix auth, protecter profile page

--- src/lib/schemas/auth.ts
+++ src/lib/schemas/auth.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod';
 
 export const signUpSchema = z.object({
-	name: z.string().min(2, "Ім'я має містити мінімум 2 символи"),
+	username: z.string().min(2, "Ім'я має містити мінімум 2 символи"),
 	email: z.string().email('Невірний формат email'),
 	password: z.string().min(8, 'Пароль має містити мінімум 8 символів'),
 	agree: z.literal(true).refine((val) => val === true, {
-    message: 'Необхідно прийняти умови використання'
-})
+		message: 'Необхідно прийняти умови використання'
+	})
+});
+
+export const signInSchema = z.object({
+	email: z.string().email('Невірний формат email'),
+	password: z.string().min(1, 'Введіть пароль')
 });
 
 export type SignUpSchema = typeof signUpSchema;
+export type SignInSchema = typeof signInSchema;
